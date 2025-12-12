a0fc881b354d34b6da1bb132789fcb7643cb0734
liu11y <114499437+liu11y@users.noreply.github.com>
2025-12-13 00:01:57 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,12 @@
-import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
-import robotsTxt from "astro-robots-txt";
-import UnoCSS from "@unocss/astro";
-import icon from "astro-icon";
-
-import solidJs from "@astrojs/solid-js";
-import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
-
-import svelte from "@astrojs/svelte";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import Unocss from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarcocavallo.com/",
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://gianmarcocavallo.com/sitemap-index.xml",
-        "https://gianmarcocavallo.com/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon(),
-    svelte(),
-  ],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
-  output: "server",
-  adapter: netlify({ edgeMiddleware: true }),
-  vite: {
-    assetsInclude: "**/*.riv",
-  },
+  // 把这里改成 static，生成纯静态网页
+  output: 'static', 
+  // 删掉了 adapter: netlify() 那一行
+  site: 'https://你的域名.pages.dev', // 这里如果你没买域名，可以先随便填，或者不填
+  integrations: [sitemap(), Unocss()],
 });
